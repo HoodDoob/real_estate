4a21ec2eb493c9338f37566801d00631dbd2bd6e
Hubert <ziemnioczek@gmail.com>
2022-03-24 13:41:28 +0100

    First commit

---
+++ javascript/index.js
@@ -0,0 +1,75 @@
+// 620cd6d634fd62156585865c
+// https://hubemmd-34fa.restdb.io/rest/bed-fashion?max=20
+
+const urlParams = new URLSearchParams(window.location.search);
+const query = urlParams.get("category");
+const options = {
+  headers: { "x-apikey": "620cd6d634fd62156585865c" },
+};
+const url = `https://hubemmd-34fa.restdb.io/rest/bed-fashion?max=20&q={"category" : {"$in" : ["${query}"]}}`;
+// const url = "https://hubemmd-34fa.restdb.io/rest/bed-fashion?max=20" + query;
+
+fetch(url, options)
+  .then((response) => {
+    if (!response.ok) {
+      throw Error(response.statusText);
+    }
+    return response.json();
+  })
+  .then((data) => {
+    console.log(data);
+    data.forEach(showProduct);
+    // showProduct(data);
+  })
+  .catch((e) => {
+    console.error("An error occured:", e.message);
+  });
+
+function showProduct(product) {
+  // grab the template\
+  const template = document.querySelector("#test123").content;
+  // clone the template
+  const copy = template.cloneNode(true);
+  // change content
+
+  document.querySelector(
+    " .breadcrumbs .breadcrumb_3"
+  ).textContent = `${query}`;
+  copy.querySelector(".description h3").textContent = product.name;
+  copy.querySelector(".description .price").textContent =
+    "DKK " + product.price + ",-";
+
+  copy.querySelector(
+    ".smallProduct img"
+  ).src = `http://hubkur.dk/images_mattress/${product._id}.jpg`;
+
+  copy
+    .querySelector(".arrow")
+    .setAttribute("href", `product.html?id=${product._id}`);
+
+  if (product.favourite == true) {
+    copy.querySelector(".favourite").classList.remove("hidden");
+  }
+
+  // grab parent
+  const parent = document.querySelector("#productlistGrid");
+  // append
+  parent.appendChild(copy);
+}
+
+//      button           button             button
+window.addEventListener("load", start_screen);
+
+function start_screen() {
+  document.querySelector("#burger_1").addEventListener("click", open_menu);
+  document.querySelector("#burgerMenu2").addEventListener("click", close_menu);
+}
+
+function open_menu() {
+  document.querySelector("#burgerMenu2").classList.remove("hidden");
+}
+function close_menu() {
+  document.querySelector("#burgerMenu2").classList.add("hidden");
+}
+
+//      button           button             button
